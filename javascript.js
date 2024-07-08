71548ae4d502fd05605bdf482696c174e5a6ef23
danilo-ck <127243735+danilo-ck@users.noreply.github.com>
2024-07-08 13:55:13 +0200

    Edit javascript.js
    This commit add a third function that takes the
    human and computer player choices as arguments,
    plays a single round, increments the round
    winner's score and logs a winner announcement.

--- javascript.js
+++ javascript.js
@@ -14,12 +14,53 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt(`Choose: “rock”, “paper” or “scissors”.`);
+    return prompt(`Write: Rock, paper or scissors.`);
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if(humanChoice === computerChoice){
+        return console.log("It's a tie!!")
+    }
+
+    switch (humanChoice) {
+        case "rock":
+            if(computerChoice === "paper"){
+                computerScore++;
+                return console.log("You lose! Paper beats Rock");
+            } else{
+                humanScore++;
+                return console.log("You win! Rock beats Scissors");
+            }
+        case "paper":
+            if(computerChoice === "scissors"){
+                computerScore++;
+                return console.log("You lose! Scissors beats Paper");
+            } else{
+                humanScore++;
+                return console.log("You win! Paper beats Rock");
+            }
+        case "scissors":
+            if(computerChoice === "rock"){
+                computerScore++;
+                return console.log("You lose! Rock beats Scissors");
+            } else{
+                humanScore++;
+                return console.log("You win! Scissors beats Paper");
+            }
+        default:
+            return console.log("Are you sure you have spelled the option correctly?")
+
+    }
+
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 
